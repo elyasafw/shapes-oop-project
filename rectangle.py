1572ac18ed16e3ec58569a5ac2c44ef825a9b087
elyasaf waisman <elyasafw1997@gmail.com>
2026-05-27 11:59:03 +0300

    added Shape & Rectangle classes

---
+++ rectangle.py
@@ -0,0 +1,15 @@
+from calculator import Shape
+
+
+class Rectangle(Shape):
+    def __init__(self):
+        super().__init__()
+
+    def get_area(self):
+        pass
+
+    def get_perimeter(self):
+        pass
+    
+    def __str__():
+        pass
