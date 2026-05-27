2a15e5580808f18ca49fca5cc66f764eebec852a
elyasaf waisman <elyasafw1997@gmail.com>
2026-05-27 15:01:06 +0300

    added triangle cless

--- square.py
+++ square.py
@@ -4,4 +4,4 @@ from rectangle import Rectangle
 class Square(Rectangle):
     def __init__(self, shape_type, side):
         super().__init__(shape_type, side, side)
-        self.side = side 
+        self.side = side
